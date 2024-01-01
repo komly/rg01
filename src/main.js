575e3aeeb2412319e070974454e7c51f69c5911e
Dmitry Petrov <d.petrov@aliexpress.ru>
2024-01-02 01:50:54 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import {Application, Sprite, Rectangle} from 'pixi.js';
+import {Application, Sprite, Graphics} from 'pixi.js';
 import {Sound} from '@pixi/sound';
 
 const app = new Application({
@@ -9,5 +9,38 @@ const app = new Application({
 document.body.appendChild(app.view);
 
 
-const sound = Sound.from('beat.wav');
-sound.play();
+var graphics = new Graphics();
+
+graphics.beginFill(0xFFFF00);
+graphics.lineStyle(5, 0xFF0000);
+graphics.drawRect(0, 0, 300, 200);
+
+app.stage.addChild(graphics);
+
+let nextTimeToBeat = 0;
+let beat = 0;
+let currentBeatTime = 0;
+
+graphics.alpha = 0.5;
+async function main() {
+    const sound = Sound.from('beat.wav');
+    const inst = await sound.play();
+    
+    const timePerBeat = 60 / 130 * 4;
+
+    app.ticker.add(dt => {
+        const time = inst._elapsed;
+        if (time >= nextTimeToBeat) {
+            currentBeatTime = beat * timePerBeat;
+            beat++;
+            nextTimeToBeat = beat * timePerBeat;
+            graphics.alpha = 1;
+        } else {
+            graphics.alpha -= 0.03 * dt;
+        }
+    
+    });
+}
+
+
+main();
